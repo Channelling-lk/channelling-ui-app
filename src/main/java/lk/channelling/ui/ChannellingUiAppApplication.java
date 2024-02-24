c697adc7135a9fc0a7434038df3306a8ea0001bc
Chinthaka Manathunga <Chinthaka.Manathunga@lseg.com>
2024-02-25 05:21:07 +0530

    Initial Changes

---
+++ src/main/java/lk/channelling/ui/ChannellingUiAppApplication.java
@@ -0,0 +1,13 @@
+package lk.channelling.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannellingUiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannellingUiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/channelling/channellinguiapp/ChannellingUiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.channelling.channellinguiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannellingUiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
