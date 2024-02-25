2b48f3f90768c293e6467710151ea96d08b698e8
Chinthaka Manathunga <Chinthaka.Manathunga@lseg.com>
2024-02-26 02:38:53 +0530

    Initial Changes

--- src/main/java/lk/channelling/ui/config/SecurityConfiguration.java
+++ src/main/java/lk/channelling/ui/config/SecurityConfiguration.java
@@ -15,28 +15,38 @@
  */
 package lk.channelling.ui.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
 public class SecurityConfiguration {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-//                        authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
-//                                .requestMatchers("/", "/home", "/about", "/login").permitAll()
-//                                .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-//                                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
-//                                .anyRequest().authenticated())
-//                .formLogin(httpSecurityFormLoginConfigurer ->
-//                        httpSecurityFormLoginConfigurer.loginPage("/login").permitAll())
-//                .logout(LogoutConfigurer::permitAll)
-//                .httpBasic(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll());
+
+        return http.build();
+    }
 
 //    @Autowired
 //    public void configurerGlobal(AuthenticationManagerBuilder auth) throws Exception {
---
+++ src/main/java/lk/channelling/ui/service/UserDetail.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 Channelling.lk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package lk.channelling.ui.service;
+
+import lk.channelling.ui.entity.User;
+import lk.channelling.ui.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@Service
+public class UserDetail implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsernameOrEmail(username, username);
+
+        if (user.isEmpty())
+            throw new UsernameNotFoundException("Incorrect user name or password");
+
+        Set<GrantedAuthority> authorities = user.get().getRoles().stream().map(role -> new SimpleGrantedAuthority((role.getName()))).collect(Collectors.toSet());
+        return new org.springframework.security.core.userdetails.User(username, user.get().getPassword(), authorities);
+
+    }
+}
